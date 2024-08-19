200fc6365c57b4339241cbec9c92ea8235f289e0
ADIB SULTHON MUAMMAL <222111840@stis.ac.id>
2024-08-19 13:13:30 +0700

    try deploy on render fix build puppeteer v16

--- puppeteer.config.js
+++
@@ -1,7 +0,0 @@
-import { join } from 'path';
-
-export default {
-  // Ubah ke cache directory yang benar
-  cacheDirectory: join(process.cwd(), '.cache', 'puppeteer'),
-  executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || null, // Atur executable path jika diperlukan
-};
